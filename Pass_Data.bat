2dc16fba5bb9330ab5780318151ec0cd5e7486de
Alex Lee <alex_bn_lee@hotmail.com>
2018-10-09 14:47:21 +1100

    1

---
+++ Pass_Data.bat
@@ -0,0 +1,7 @@
+@echo  off
+
+pscp.exe -pw lj666yjh D:\FileTransfer\Data\* ocean@192.168.14.28:/home/ocean/alex/FileTransfer/Data
+
+echo Ö´ÐÐ´úÂë¡¾pass_data.sh¡¿
+
+putty.exe -pw lj666yjh ocean@192.168.14.28
